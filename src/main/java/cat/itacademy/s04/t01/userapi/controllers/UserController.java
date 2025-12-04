44f731fa50b4822e9fc15d3949fb7d3560f39d78
Albert Medina <albertmecu@gmail.com>
2025-12-04 11:54:40 +0100

    feat: added get endpoint for a particular user given its id

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,10 +1,8 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
+import cat.itacademy.s04.t01.userapi.exceptions.UserNotFoundException;
 import cat.itacademy.s04.t01.userapi.models.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,5 +23,10 @@ public class UserController {
         User newUser = new User(UUID.randomUUID(), userRequest.name(), userRequest.email());
         users.add(newUser);
         return newUser;
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable UUID id) {
+        return users.stream().filter(user -> user.id().equals(id)).findFirst().orElseThrow(() -> new UserNotFoundException("User " + id + " not found"));
     }
 }
