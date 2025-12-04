41a6bb3a4dd0a9ac1bb00dafcabfa559db226b10
Albert Medina <albertmecu@gmail.com>
2025-12-04 11:30:02 +0100

    feat: added endpoint for users

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,11 +1,19 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class UserController {
 
-    public static final List<User> users = new ArrayList<>();
+    private static final List<User> users = new ArrayList<>();
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return users;
+    }
 }
