97426b48d12719c54974cf059f54b55a375f16ba
Albert Medina <albertmecu@gmail.com>
2025-12-04 11:26:43 +0100

    feat: added User and UserController classes

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -0,0 +1,11 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+import cat.itacademy.s04.t01.userapi.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserController {
+
+    public static final List<User> users = new ArrayList<>();
+}
---
+++ src/main/java/cat/itacademy/s04/t01/userapi/models/User.java
@@ -0,0 +1,10 @@
+package cat.itacademy.s04.t01.userapi.models;
+
+import java.util.UUID;
+
+public class User {
+
+    private UUID id;
+    private String name;
+    private String email;
+}
